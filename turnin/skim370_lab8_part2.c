8a9625a71757b83aa910276fb57a771007cc2f93
Skylar Kim <skim370@wch136-02.cs.ucr.edu>
2019-11-06 09:57:35 -0800

    turnin

---
+++ turnin/skim370_lab8_part2.c
@@ -0,0 +1,28 @@
+/*
+ * lab8.c
+ *
+ * Created: 11/4/2019 10:58:35 AM
+ * Author : skylar kim
+ */ 
+
+#include <avr/io.h>
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+int main(void)
+{
+   	DDRA = 0x00; PORTA = 0xFF; 
+    DDRB = 0xFF; PORTB = 0x00; 
+    DDRD = 0xFF; PORTD = 0x00;
+	
+	ADC_init();
+    while (1) 
+    {
+		unsigned short temp = ~ADC;
+		unsigned char top8 = (char)temp;
+		unsigned char bottom2 = (char)(temp >> 8);
+		PORTB = top8;
+		PORTD = bottom2;
+    }
+}
